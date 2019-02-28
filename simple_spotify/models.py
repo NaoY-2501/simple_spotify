17e3a418d53ecdaa53d70a3aa179c65c573a9dfe
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-03-01 00:12:33 +0900

    wip. classes fix

--- simple_spotify/models.py
+++ simple_spotify/models.py
@@ -4,14 +4,13 @@ from .consts import RESULT_TYPES
 class Artist:
     def __init__(self, artist_json):
         self.external_urls = artist_json['external_urls']
-        self.followers = Followers(artist_json['followers'])
         self.genres = artist_json['genres']
         self.href = artist_json['href']
         self.artist_id = artist_json['id']
-        self.images = [Image(img) for img in artist_json['images']]
         self.name = artist_json['name']
         self.popularity = artist_json['popularity']
         self.uri = artist_json['uri']
+        self.raw = artist_json
 
     def __str__(self):
         return self.name
@@ -20,20 +19,23 @@ class Artist:
     def raw_to_object(cls, raw):
         return cls(raw)
 
-    def get_images(self):
-        """
-        Get image of artist as generator.
-        :return: generator of Image object
-        """
-        for image in self.images:
-            yield image
+    @property
+    def followers(self):
+        followers = self.raw['followers']
+        return followers['total']
 
-
-class Followers:
-    def __init__(self, followers):
+    @property
+    def followers_href(self):
         # href is always set to null as the Spotify Web API does not support it at the moment.
-        self.href = followers['href'] if followers['href'] != 'null' else None
-        self.total = followers['total']
+        followers = self.raw['followers']
+        return followers['href'] if followers['href'] != 'null' else None
+
+    @property
+    def images(self):
+        images = []
+        for image in self.raw['images']:
+            images.append(Image(image))
+        return images
 
 
 class Image:
@@ -47,10 +49,10 @@ class Image:
 
 
 class SearchResult:
-    def __init__(self, q, search_type, raw):
+    def __init__(self, q, search_type, result_json):
         self.q = q
         self.search_type = search_type
-        self.raw = raw
+        self.raw = result_json
 
     def __str__(self):
         return 'Query:{q} Result:{search_type}'.format(q=self.q, search_type=self.search_type)
@@ -64,7 +66,7 @@ class SearchResult:
     @property
     def artists(self):
         if 'artist' in self.search_type:
-            return SearchResultDetail(self.raw[RESULT_TYPES['artist']])
+            return SearchResultDetail(self.raw[RESULT_TYPES['artist']], Artist)
         return None
 
     @property
@@ -82,18 +84,23 @@ class SearchResult:
 
 # TODO: add next() and previous() as property.
 class SearchResultDetail:
-    def __init__(self, result_json):
+    def __init__(self, result_json, klass=None):
         self.href = result_json['href']
-        self.items = result_json['items']
         self.limit = result_json['limit']
         self.offset = result_json['offset']
         self.previous = result_json['previous']
         self.next = result_json['next']
         self.total = result_json['total']
+        self.raw = result_json
+        self.__klass = klass
 
     def __str__(self):
         return self.href
 
-    def get_item(self):
-        for item in self.items:
-            yield item
+    @property
+    def items(self):
+        for item in self.raw['items']:
+            if self.__klass:
+                yield self.__klass(item)
+            else:
+                yield item
