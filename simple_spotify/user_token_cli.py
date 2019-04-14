705cc240ca3e191d85513857ed74b225555888f9
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-04-14 15:57:22 +0900

    wip. make script for get access token

---
+++ simple_spotify/user_token_cli.py
@@ -0,0 +1,52 @@
+import base64
+import json
+import random
+import urllib.parse
+import urllib.request
+import webbrowser
+import string
+
+ENDPOINT = 'https://accounts.spotify.com/authorize'
+
+
+def main():
+    logo = """
+    ======================================
+    Simple-Spotify
+    Authorization Code Get Client Tool
+    ======================================
+    """
+    print(logo)
+
+    client_id = input('Enter your Client ID :')
+
+    redirect_uri = input('Enter redirect URI :')
+
+    scopes = []
+    cont = 'y'
+    while cont == 'y':
+        scope = input('Scope :')
+        cont = input('Add more scope?(y/n) :')
+        scopes.append(scope)
+
+    state = input('Enter the state (Optional, but strongly recommended) :')
+
+    data = {
+        'client_id': client_id,
+        'response_type': 'code',
+        'redirect_uri': redirect_uri,
+        'scope': ' '.join(scopes),
+        'show_dialog': True,
+    }
+
+    if state:
+        data['state'] = state
+
+    params = urllib.parse.urlencode(data)
+
+    url = f'{ENDPOINT}?{params}'
+    print(url)
+
+
+if __name__ == '__main__':
+    main()
