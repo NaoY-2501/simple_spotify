dc20401b4594edb95b561ea78efb06cc8e5623aa
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-04-28 01:41:55 +0900

    implement output authorization parameters file

--- simple_spotify/sp_auth.py
+++ simple_spotify/sp_auth.py
@@ -1,18 +1,13 @@
+import json
+import pprint
 import urllib.parse
 import urllib.request
+import webbrowser
 
 ENDPOINT = 'https://accounts.spotify.com/authorize'
 
 
-def main():
-    logo = """
-    ======================================
-    Simple-Spotify
-    Authorization Code Get Client Tool
-    ======================================
-    """
-    print(logo)
-
+def input_params():
     client_id = input('Enter your Client ID :')
 
     redirect_uri = input('Enter redirect URI :')
@@ -36,11 +31,42 @@ def main():
 
     if state:
         data['state'] = state
+    return data
+
+
+def main():
+    logo = """
+    ======================================
+    Simple-Spotify
+    Authorization Code Get Client Tool
+    ======================================
+    """
+    print(logo)
+
+    data = input_params()
 
     params = urllib.parse.urlencode(data)
 
     url = f'{ENDPOINT}?{params}'
-    print(url)
+
+    webbrowser.open(url)
+
+    redirected_url = input('Enter the redirected URL:')
+    splited_url = urllib.parse.urlsplit(redirected_url)
+    code = urllib.parse.parse_qs(splited_url.query)['code'][0]
+
+    auth_params = {
+        'client_id': data['client_id'],
+        'client_secret': data['client_secret'],
+        'redirect_uri': data['redirect_uri'],
+        'code': code,
+    }
+
+    pprint.pprint(auth_params)
+    print('Output authorization params...')
+    with open('authorization_params.json', 'w') as f:
+        json.dump(auth_params, f)
+    print('Done !')
 
 
 if __name__ == '__main__':
