8f0f0b1eae1da41f0fff496285be797049372bd8
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-05-01 11:10:32 +0900

    fix AutorizationCodeFlow and add decorator for refresh_token.

--- simple_spotify/authorization.py
+++ simple_spotify/authorization.py
@@ -4,13 +4,15 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
+from datetime import datetime
+
 
 ENDPOINT_TOKEN = 'https://accounts.spotify.com/api/token'
 
 
 class SpotifyAuthBase:
     @classmethod
-    def get_authorization(cls, client_id, client_secret):
+    def get_header_param(cls, client_id, client_secret):
         base64string = base64.encodebytes('{client_id}:{client_secret}'.format(
              client_id=client_id,
              client_secret=client_secret
@@ -23,11 +25,8 @@ class SpotifyAuthBase:
 
     @classmethod
     def get_response(cls, headers, request_body):
-        req = urllib.request.Request(
-            ENDPOINT_TOKEN,
-            urllib.parse.urlencode(request_body).encode('ascii'),
-            headers=headers
-        )
+        data = urllib.parse.urlencode(request_body).encode('ascii')
+        req = urllib.request.Request(ENDPOINT_TOKEN, data, headers=headers)
         try:
             with urllib.request.urlopen(req) as res:
                 res = json.loads(res.read().decode('utf-8'))
@@ -40,19 +39,21 @@ class ClientCredentialsFlow(SpotifyAuthBase):
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret
-        self.access_token = self.response['access_token']
-        self.token_type = self.response['token_type']
-        self.expires_in = self.response['expires_in']
+        self.access_token = None
+        self.token_type = None
+        self.expires_in = None
 
     @property
     def authorization(self):
         return {'Authorization': 'Bearer {}'.format(self.access_token)}
 
-    @property
-    def response(self):
-        headers = self.get_authorization(self.client_id, self.client_secret)
+    def token_request(self):
+        headers = self.get_header_param(self.client_id, self.client_secret)
         request_body = {'grant_type': 'client_credentials'}
-        return self.get_response(headers, request_body)
+        response = self.get_response(headers, request_body)
+        self.access_token = response['access_token']
+        self.token_type = response['token_type']
+        self.expires_in = response['expires_in']
 
 
 class AuthorizationCodeFlow(SpotifyAuthBase):
@@ -60,31 +61,39 @@ class AuthorizationCodeFlow(SpotifyAuthBase):
         self.client_id = client_id
         self.client_secret = client_secret
         self.code = code
+        self.created_at = datetime.now()
         self.redirect_uri = redirect_uri
-        self.access_token = self.response['access_token']
-        self.token_type = self.response['token_type']
-        self.expires_in = self.response['expires_in']
-        self.scope = self.response['scope']
-        self.refresh_token = self.response['refresh_token']
+        self.access_token = None
+        self.token_type = None
+        self.expires_in = None
+        self.scope = None
+        self.refresh_token = None
 
     @property
     def authorization(self):
         return {'Authorization': 'Bearer {}'.format(self.access_token)}
 
-    @property
-    def response(self):
-        headers = self.get_authorization(self.client_id, self.client_secret)
+    def token_request(self):
+        headers = self.get_header_param(self.client_id, self.client_secret)
         request_body = {
             'grant_type': 'authorization_code',
             'code': self.code,
-            'redirect_uri': self.redirect_uri
+            'redirect_uri': self.redirect_uri,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
         }
-        return self.get_response(headers, request_body)
+        response = self.get_response(headers, request_body)
+        self.access_token = response['access_token']
+        self.token_type = response['token_type']
+        self.expires_in = response['expires_in']
+        self.scope = response['scope']
+        self.refresh_token = response['refresh_token']
 
     def token_refresh(self):
-        headers = self.get_authorization
+        headers = self.get_header_param(self.client_id, self.client_secret)
         request_body = {
             'grant_type': 'refresh_token',
             'refresh_token': self.refresh_token,
         }
-        self.access_token = self.get_response(headers, request_body)['refresh_token']
+        self.access_token = self.get_response(headers, request_body)['access_token']
+        self.created_at = datetime.now()
--- simple_spotify/decorators.py
+++ simple_spotify/decorators.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+from .authorization import AuthorizationCodeFlow
 from .errors import SpotifyIdsNotAssignedError
 
 
@@ -7,5 +10,18 @@ def has_ids(func):
         if not spotify_ids:
             raise SpotifyIdsNotAssignedError
         result = func(self, spotify_ids, **kwargs)
+        return result
+    return wrapper
+
+
+def token_refresh(func):
+
+    def wrapper(self, *args, **kwargs):
+        auth = self.authorization
+        if isinstance(auth, AuthorizationCodeFlow):
+            now = datetime.now()
+            if auth.created_at + timedelta(seconds=auth.expires_in) < now:
+                auth.token_refresh()
+        result = func(self, *args, **kwargs)
         return result
     return wrapper
