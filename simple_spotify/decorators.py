5d202fdbb7a6b38b23c16bed169d3af374851bf7
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-05-01 17:43:22 +0900

    add ids_validation

--- simple_spotify/decorators.py
+++ simple_spotify/decorators.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 from .authorization import AuthorizationCodeFlow
-from .errors import SpotifyIdsNotAssignedError
+from .errors import SpotifyIdsNotAssignedError, QueryValidationError
 
 
 def has_ids(func):
@@ -11,6 +11,21 @@ def has_ids(func):
             raise SpotifyIdsNotAssignedError
         result = func(self, spotify_ids, **kwargs)
         return result
+    return wrapper
+
+
+def ids_validation(func):
+
+    def wrapper(self, ids=None, **kwargs):
+        # query validation
+        if not isinstance(ids, list):
+            raise QueryValidationError('IDs must be list.')
+        if len(ids) > 50:
+            raise QueryValidationError('Too many ids. Maximum length is 50.')
+        for each in ids:
+            if not isinstance(each, str):
+                raise QueryValidationError('ID must be str.')
+        return func(self, ids, **kwargs)
     return wrapper
 
 
