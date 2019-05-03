6f93c4b3749d51a08666485a2e48d763a7f0e04f
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-05-03 16:14:28 +0900

    add exception

--- simple_spotify/errors.py
+++ simple_spotify/errors.py
@@ -24,3 +24,11 @@ class SpotifyIdsNotAssignedError(Exception):
 
     def __str__(self):
         return self.reason
+
+
+class PathParameterError(Exception):
+    def __init__(self, reason):
+        self.reason = reason
+
+    def __str__(self):
+        return self.reason
